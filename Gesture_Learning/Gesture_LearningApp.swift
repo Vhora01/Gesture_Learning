7bfebd51bd48703418b0800c2354e90d2b8ffa5e
Prakash <vhoraprakash007@gmail.com>
2026-04-13 16:44:17 +0530

    Initial Commit

---
+++ Gesture_Learning/Gesture_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gesture_LearningApp.swift
+//  Gesture_Learning
+//
+//  Created by Prakash on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Gesture_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
